0c99eef0413a6f293c6b6cff9c9ed228dc346ea5
username <jcaputo66@aol.com>
2022-05-06 14:09:44 -0700

    lab complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+class Seed:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, season):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.season = season
+
+seeds = [
+  Seed('Carrot', 'root vegetable', 'sweet and crucnhy', 'spring'),
+  Seed('Beet', 'root vegetable', 'colorful, dynamic, easy to grow', 'fall and spring'),
+  Seed('Broccoli', 'cruciferous vegetable', 'slow growing but good for the soul', 'spring')
+]
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Sow Dem Seeds</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def seeds_index(request):
+  return render(request, 'seeds/index.html', { 'seeds': seeds })
